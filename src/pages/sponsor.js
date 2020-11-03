ce3ec36a47b20e3d89dc0b3d4a60759c3b307505
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-11-03 21:38:31 +0100

    sponsor

---
+++ src/pages/sponsor.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function Sponsor(props) {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
+  return (
+    <main>
+      <h1>Sponsorship tinyreact</h1>
+      <p>Stats for September:</p>
+      <ul>
+        <li>4 issues</li>
+        <li>2750 subscribers</li>
+        <li>44% open rate</li>
+        <li>= 4,840 impressions, 1210 unique</li>
+      </ul>
+      <br />
+      <p>Cost: $300 for the month = 4 issues</p>
+      <br />
+      <p>
+        I try to keep the sponsor copy simple like the rest of the newsletter,{" "}
+        <a href="https://tinyreact.email/2020-10-20">here's an example</a>.
+      </p>
+      <p>
+        If you have any questions, feel free to reach out to me via{" "}
+        <a href="https://sid.st/email">email</a> or{" "}
+        <a href="https://twitter.com/siddharthkp">twitter DM</a>
+      </p>
+    </main>
+  )
+}
+
+export default Sponsor
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
